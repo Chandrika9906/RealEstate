4c593103b3f954e4cc87d18e7ac6a629369a68ea
Chandrika9906 <chandrika992006@gmail.com>
2026-08-09 10:43:39 +0530

    till user model

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,9 @@
 import mongoose  from "mongoose";
 export const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://<db_username>:I3rBqHlE5AsYFXBQ@cluster0.sbvep9i.mongodb.net/RealEstate").then(()=>{
-        console.log("MongoDB Connected");
-    })
+    await mongoose.connect(
+            "mongodb+srv://chandrika992006_db_user:Chandusree@cluster0.sbvep9i.mongodb.net/RealEstate"
+        ).then(()=>{
+            console.log("DB CONNECTED");
+        })
 }
+ 
